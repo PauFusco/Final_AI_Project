636ea27660f6e0fb1e0cba470cd4e23bb2caeddc
PauFusco <99730051+PauFusco@users.noreply.github.com>
2024-01-14 20:51:56 +0100

    hide of the robber fixed

---
+++ Assets/Scripts/Perception/Moves.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Moves : MonoBehaviour
+{
+    private float radius = 1;
+    private float offset = 1;
+
+    private NavMeshHit hit;
+    private Vector3 tempTarget;
+    private Vector3 worldTarget;
+
+    public NavMeshAgent agent;
+    public GameObject objective;
+
+    public GameObject[] obstacles;
+
+    public void Wander()
+    {
+        tempTarget = Random.insideUnitCircle * radius;
+        tempTarget += new Vector3(0, 0, offset);
+        worldTarget = transform.TransformPoint(tempTarget);
+        worldTarget.y = 0f;
+        if (NavMesh.SamplePosition(worldTarget, out hit, 1.0f, NavMesh.AllAreas)) agent.destination = hit.position;
+    }
+
+    public void Seek(Vector3 position)
+    {
+        agent.destination = position;
+    }
+
+    public void Hide(NavMeshAgent hideFrom)
+    {
+        GameObject closestObj = null;
+        float closestDist = -1;
+        foreach (GameObject item in obstacles)
+        {
+            float temp = Vector3.Distance(hideFrom.transform.position, item.transform.position);
+            if (closestDist == -1 || temp < closestDist)
+            {
+                closestObj = item;
+                closestDist = temp;
+            }
+        }
+        float RelDist = 1.65f / closestDist;
+        Vector3 lerpPoint = Vector3.Lerp(closestObj.transform.position, hideFrom.transform.position, RelDist);
+
+        agent.destination = closestObj.transform.position + (closestObj.transform.position - lerpPoint);
+    }
+}
--- Assets/Scripts/Perception/Robber.cs
+++ Assets/Scripts/Perception/Robber.cs
@@ -1,18 +1,109 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Robber : MonoBehaviour
 {
-    private Wander wanderScript;
+    public GameObject[] grannys;
+    public GameObject[] trees;
+    public GameObject policeman;
 
-    // Start is called before the first frame update
-    private void Start()
+    public NavMeshAgent Policeman;
+
+    private GameObject target;
+
+    private float dist2Steal = 1f;
+    private float dist2Cop = 5f;
+    private Moves moves;
+    private NavMeshAgent agent;
+
+    private bool stolen = false;
+    private WaitForSeconds wait = new WaitForSeconds(0.05f); // == 1/20
+
+    private delegate IEnumerator State();
+
+    private States state;
+
+    private enum States
     {
+        Wander,
+        Seek,
+        Hide
     }
 
-    // Update is called once per frame
-    private void Update()
+    private void Start()
     {
+        moves = gameObject.GetComponent<Moves>();
+        agent = gameObject.GetComponent<NavMeshAgent>();
+
+        agent.speed = 2.6f;
+
+        state = States.Wander;
+
+        target = grannys[Random.Range(0, grannys.Length - 1)];
+
+        moves.objective = target;
+
+        StartCoroutine(switchState());
+    }
+
+    private IEnumerator switchState()
+    {
+        while (true)
+        {
+            switch (state)
+            {
+                case States.Wander:
+                    state = IsGuarded() ? States.Wander : States.Seek;
+                    yield return Wander();
+                    break;
+
+                case States.Seek:
+                    state = IsGuarded() ? States.Wander : States.Seek;
+                    if (CanSteal() && !stolen)
+                    {
+                        Debug.Log("Can steal");
+                        //treasure.SetActive(false);
+                        state = States.Hide;
+                    }
+                    yield return Approaching();
+                    break;
+
+                case States.Hide:
+                    agent.speed = 2f;
+                    yield return Hiding();
+                    break;
+            }
+            yield return switchState();
+        }
+    }
+
+    private IEnumerator Wander()
+    {
+        moves.Wander();
+        yield return null;
+    }
+
+    private IEnumerator Approaching()
+    {
+        moves.Seek(target.transform.position);
+        yield return null;
+    }
+
+    private IEnumerator Hiding()
+    {
+        moves.Hide(Policeman);
+        yield return null;
+    }
+
+    private bool IsGuarded()
+    {
+        return (Vector3.Distance(Policeman.transform.position, target.transform.position) <= dist2Cop);
+    }
+
+    private bool CanSteal()
+    {
+        return (Vector3.Distance(agent.transform.position, target.transform.position) <= dist2Steal);
     }
 }
